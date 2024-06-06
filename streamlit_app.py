925b5c6fb38db3f6e711e5a097f1c631f7ce2c02
aswathi-swaminathan1 <149940799+aswathi-swaminathan1@users.noreply.github.com>
2024-06-06 16:47:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title("My Parents New Healthy Dinner")
+streamlit.title("Breakfast Menu")
+streamlit.text("Omega 3 & Blueberry Oatmeal")
+streamlit.text("kale, Spinach & rocket Smoothie")
+streamlit.text("Hard-boiled Free-Range Egg")
