fb85ed50901adbd921cd014c8b7a06c5ae918825
aswathi-swaminathan1 <149940799+aswathi-swaminathan1@users.noreply.github.com>
2024-06-06 17:01:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,8 @@ streamlit.text("🥗 kale, Spinach & rocket Smoothie")
 streamlit.text("🐔 Hard-boiled Free-Range Egg")
 streamlit.text("🥑🍞 Avacado Toast")
 
-streamlit.title("Build Your Own Fruit Smoothie")
-
+streamlit.title("🍌🥭 Build Your Own Fruit Smoothie 🥝 🍇")
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
 
